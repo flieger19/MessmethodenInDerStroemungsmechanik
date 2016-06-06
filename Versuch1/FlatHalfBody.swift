15477086e7f822db0e8a6a2db5889ab8661db7eb
Constantin Dullo <constantin.dullo@gmail.com>
2016-06-06 09:47:15 +0200

    Hinzufügen der Klasse: FlatHalfBody

---
+++ Versuch1/FlatHalfBody.swift
@@ -0,0 +1,21 @@
+//
+//  FlatHalfBody.swift
+//  Versuch1
+//
+//  Created by Constantin Dullo on 06.06.16.
+//  Copyright © 2016 Constantin Dullo. All rights reserved.
+//
+
+import Foundation
+
+class FlatHalfBody: NSObject {
+    
+    // variables
+    
+    // constructor
+    override init() {
+        super.init()
+    }
+    
+    // methods
+}
